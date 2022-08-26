4c3aab97bb15df7aa846a1c301fb5f53d10f6cc3
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2022-08-27 02:02:26 +0300

    restarted with fresh template
    restarted with fresh template

---
+++ src/js/refs.js
@@ -0,0 +1,18 @@
+// const gallery = document.querySelector('.js-gallery');
+const productBox = document.querySelector('.box');
+const productButton = document.querySelector('.button');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const modalCounter = document.querySelector('.modal_counter');
+const modalClearCounter = document.querySelector('.modal_clear-couter');
+const closeButton = document.querySelector('.modal_close-btn');
+export default {
+  // gallery,
+  productBox,
+  productButton,
+  overlay,
+  modal,
+  modalCounter,
+  modalClearCounter,
+  closeButton,
+};
